70a000837550f55c0a8d8142d58493bd9e22f3e3
shaojundao <372248040@qq.com>
2020-02-25 15:14:23 +0800

    首页开发第一天

---
+++ src/vue.config.js
@@ -0,0 +1,29 @@
+// module.exports = {
+//     configureWebpack: {
+//         resolve: {
+//             alias: {
+//                 'assets': '@/assets',
+//                 'common': '@/common',
+//                 'components': '@/components',
+//                 'network': '@/network',
+//                 'views': '@/views',
+//             }
+//         }
+//     }
+// }
+// const path = require('path')
+// function resolve(dir){
+//     return path.join(__dirname,dir)
+// }
+
+// module.exports = {
+//     lintOnSave: true,
+//     chainWebpack:(config)=>{
+//         config.resolve.alias
+//             .set('assets',resolve('src/assets'))
+//             .set('common',resolve('src/common'))
+//             .set('components',resolve('src/components'))
+//             .set('network',resolve('src/network'))
+//             .set('views',resolve('src/views'));
+//     }
+// }
